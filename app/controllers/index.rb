213eb46ae08c6bc13eb01ba4dc657f74096182a6
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-30 12:34:28 -0500

    initial commit

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,17 @@
 get '/' do
+  @grandma = params[:grandma]
+  @yoohoo = params[:yoohoo]
   # Look in app/views/index.erb
+  erb :index
+end
+
+post '/grandma' do
+  if params[:user_input].upcase == params[:user_input]
+    @grandma = "ALL DAY LONG!"
+  else
+    @grandma = "SPEAK UP SONNY!"
+  end
+  #"<br>Params: <code>#{params.inspect}</code>"
+
   erb :index
 end
